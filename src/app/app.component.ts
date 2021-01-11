8cc7b81889e285b287f3bf1ca8f64399cd56b1e9
Marcin Medryk <medryk.kontakt@gmail.com>
2021-01-11 21:43:39 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularTest';
-  supply = {amount: 1000, price: 0}
+  supply = {amount: 100, price: 2.50, wanted: 0}
+  stocked = 0
   queue = []
-  xxx = 0
 
-  clickerFunction = () => {
-    this.queue.push(this.supply)
-    console.log(this.supply)
+  addToStorage = () => {
+    if(this.supply.amount > 0 && this.supply.amount % 100 === 0 && this.supply.price > 0 && this.supply.price % 0.1 == 0) {
+      this.queue.push({amount: this.supply.amount, price: this.supply.price})
+      this.stocked = this.supply.amount + this.stocked
+      console.log(this.stocked)
+      this.supply.amount = 100
+      this.supply.price = 2.50
+    }
   }
-
-  inputChange = () => {
-    console.log(this.xxx)
-    this.supply.amount = 1
+  removeFromStorage = () => {
+    if(this.supply.wanted > 0 && this.supply.wanted % 100 === 0) {
+      this.stocked = this.stocked - this.supply.wanted
+    }
   }
 }
